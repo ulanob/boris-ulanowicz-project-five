534357c2690f4ffaa99be9033b5a629ed0343ac2
Boris <ulanowicz_b@hotmail.com>
2020-11-28 18:10:00 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import logo from './assets/endangerment.png';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      questionsArray: []
+    }
+  }
+
+  componentDidMount() {
+    axios({
+      url: `http://jservice.io/api/random`,
+      method: `GET`,
+      responseType: `json`,
+      params: {
+        count: 20
+      }
+    })
+      .then((res) => {
+        this.setState({
+          questionsArray: res.data
+        })
+        // console.log(this.state.questionsArray)
+      })
+  }
+
+  handleClick = () => {
+    console.log('clicked')
+    return (
+      <p>Hello</p>
+    )
+  }
+
+  formatValue = (value) => {
+    if (value == null) {
+      return value = 200
+    } else {
+      return value
+    }
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <img src={logo} alt="the famous endangerment logo" />
+        {
+          this.state.questionsArray.map((i) => {
+            return (
+              <div key="this.state.questionsArray.id" className="questionBox">
+                <p onClick={this.handleClick}>${this.formatValue(i.value)}</p>
+              </div>
+            )
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
