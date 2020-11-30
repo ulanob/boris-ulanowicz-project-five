c9df7807912e9adbd3309e391b4014118f5a6a1b
Boris <ulanowicz_b@hotmail.com>
2020-11-29 17:41:06 -0800

    added background, input for userName

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 import logo from './assets/endangerment.png';
-import Input from "./Input.js"
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       questionsArray: [],
+      userName: '',
       userScore: 0,
-      isQuestionSelected: false
+      // isQuestionSelected: false
     }
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
@@ -25,7 +26,6 @@ class App extends Component {
     })
       .then((res) => {
         let fixQuestionsArray = res.data;
-
         // some question objects have a value of null, so changing them to 200
         fixQuestionsArray.filter((object) => {
           if (object.value == null) {
@@ -35,37 +35,49 @@ class App extends Component {
             questionsArray: fixQuestionsArray
           })
         })
-        console.log(this.state.questionsArray);
+        // console.log(this.state.questionsArray);
       })
   }
+  getName = () => {
+    // this.setState({
+    //   userName = 
+    // })
+  }
 
-
-
-  handleClick = () => {
-    console.log('clicked', this);
-    this.setState({
-      isQuestionSelected: !this.state.isQuestionSelected
-    })
+  handleClick = (e, data) => {
+    console.log('clicked');
+    console.log(this);
+    console.log(e, data);
+    // find question that was clicked on
+    // append question, and a form that takes user input
   }
 
   render() {
     return (
       <div className="App">
         <img src={logo} alt="logo from the famous household game show 'Endangerment'" />
+        <form action="">
+          <label htmlFor="getUserName" onClick={this.getName}>What is your Name</label>
+          <input type="text" name="userName"/>
+          <button type="submit"></button>
+        </form>
         <div className="gameBoard">
           {
-            this.state.questionsArray.map((gameQuestions) => {
+            this.state.questionsArray.map((gameQuestion) => {
               return (
                 <div
-                  key={gameQuestions.id} 
+                  key={gameQuestion.id}
                   className="question"
-                  onClick={this.handleClick}>
-                  <p>${gameQuestions.value}</p>
-                  <p>{gameQuestions.category.title}</p> 
+                  onClick={this.handleClick, console.log(this)}>
+                  <p><span>$</span>{gameQuestion.value}</p>
+                  <p>{gameQuestion.category.title}</p>
                 </div>
               )
             })
           }
+        </div>
+        <div className="userInput">
+
         </div>
       </div>
     );
